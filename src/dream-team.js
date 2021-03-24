58ebbeebf4e36002fcc28b8dd1bc8892b64c1c66
Pavel Suntsou <pasha2010.94@gmail.com>
2021-03-24 21:32:47 +0300

    feat:complite 4 tasks

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  
+  if(!Array.isArray(members))
+  return false;
+
+  if (Array.isArray(members)) {
+    let dreamTeam = [];
+
+    members.forEach(i => {
+      if (typeof i === 'string')
+        dreamTeam.push(i.trim()[0].toUpperCase())
+    });
+    
+    return dreamTeam.sort().join('');
+  }
 };
--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,22 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let result = '';
+  separator = String(options.separator || '+');
+  repeatTimes = options.repeatTimes || 1;
+  addition = ('addition' in options) ? String(options.addition) : '';
+  additionRepeatTimes = options.additionRepeatTimes || 1;
+  additionSeparator = String(options.additionSeparator || '|');
+
+    for (let i=0;i<repeatTimes;i++) {
+      result+=str;
+      for (let j=0;j<additionRepeatTimes;j++) {
+        result+=addition+additionSeparator;
+      }
+      if(additionSeparator!='') result = result.slice(0,'-'+additionSeparator.length);
+      result += separator;
+    }
+    
+    return result.slice(0,'-'+separator.length);
 };
   
